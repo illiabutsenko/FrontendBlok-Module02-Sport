6c86368951f123d0daf180c0cec937222cbe3aae
illiabutsenko <ilya1990.0826@gmail.com>
2024-05-21 16:49:04 +0200

    add JavaScript, Media-adapting 768

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+// Smooth Scroll
+      
+const links = document.querySelectorAll(".hero-arrow");
+
+for (const link of links) {
+link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+e.preventDefault();
+const href = this.getAttribute("href");
+
+document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+});
+}
+
+ // Menu
+
+ function openModal() {
+    document.getElementById("modal").style.top = "0px";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-400px";
+}
